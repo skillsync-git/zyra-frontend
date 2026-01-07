a7dd8d13cf43432b3ca55a19e1feac1801b985f4
skillsync <skillsync362@gmail.com>
2026-01-07 16:24:57 +0530

    Changed adminco

--- src/app/adminco/page.js
+++ src/app/adminco/page.js
@@ -1,12 +1,12 @@
 import { Suspense } from "react";
-import AdmincoClientClient from "./AdmincoClient";
+import AdmincoClient from "./AdmincoClient";
 
 export const dynamic = "force-dynamic";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Searching...</div>}>
-      <AdmincoClientClient />
+      <AdmincoClient />
     </Suspense>
   );
 }
